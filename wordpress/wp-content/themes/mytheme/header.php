b32d85a57ec0c13df43ba3777875157064a48542
Nha Phuong <20211tt1169@mail.tdc.edu.vn>
2022-04-22 07:58:57 +0700

    update logo and

--- wordpress/wp-content/themes/mytheme/header.php
+++ wordpress/wp-content/themes/mytheme/header.php
@@ -1,15 +1,28 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-   <meta charset="<?php bloginfo( 'charset' ); ?>" >
-   <?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?> >
+
+<body <?php body_class(); 
+?>>
     <!--container-->
     <div id="container">
-  <header id="header">
-    <!-- logo -->
-   <?php mytheme_logo(); ?>
-   <!-- menu -->
-   <?php mytheme_menu('my-custom-menu' ); ?>
-  </header>
+        <header id="header">
+            <!-- Logo: hello -->
+            <?php mytheme_logo(); ?>
+
+            <!-- Menu -->
+            <div class="wrap-menu">
+            <?php 
+             mytheme_menu('my-custom-menu'); 
+            ?>
+            </div>
+  
+        </header>
--- wordpress/wp-content/themes/mytheme/single.php
+++ wordpress/wp-content/themes/mytheme/single.php
@@ -3,8 +3,8 @@ get_header();
 get_sidebar();
 $args = array (
     'post_type'=>'post', //loai la post
-    // 'orderby' => 'rand', //lay ngau nhien
-    'posts_per_page' => 5 //lay 5 bai cho 1 trang
+    'orderby' => 'rand', //lay ngau nhien
+    'posts_per_page' => 3 //lay 5 bai cho 1 trang
 );
 
 $the_query = new WP_Query($args);
@@ -23,26 +23,14 @@ if($the_query->have_posts()):
     echo '<div style = "background: pink">';
     while($the_query->have_posts()):
         $the_query->the_post();
-        echo the_title('<h4>','</h4>');
-        //get_the_author->return. the_author: in truc tiep
-        echo get_the_author();
+        echo the_title('<h1 style="text-transform:uppercase;>','</h1');
         echo the_date();
-        echo '<a href="'.get_permalink( get_the_ID() ).'">'.get_the_title().'</a>';
+        //get_the_author->return. the_author: in truc tiep
+        echo the_author();
+        echo the_date();
         echo the_content('<br><hr>');
 endwhile;
 echo '</div>';
 endif;
-
-
-	// Load posts loop.
-	while ( have_posts() ) {
-		the_post();
-
-		get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) );
-	}
-
-	// Previous/next page navigation.
-	// twenty_twenty_one_the_posts_navigation();
-
 
 get_footer();
