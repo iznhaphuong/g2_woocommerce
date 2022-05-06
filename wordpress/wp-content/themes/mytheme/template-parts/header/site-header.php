a576dd79df49f1522f0ae88083c584e42944e827
Nha Phuong <20211tt1169@mail.tdc.edu.vn>
2022-05-06 07:07:04 +0700

    update sale list, latest list

---
+++ wordpress/wp-content/themes/mytheme/template-parts/header/site-header.php
@@ -0,0 +1,10 @@
+<!-- Display site header -->
+<header id="header">
+            <!-- Logo: hello -->
+            <?php mytheme_logo(); ?>
+
+            <!-- Menu -->
+            <?php ?>
+            <?php get_template_part( 'template-parts/header/site-menu' ); ?>
+
+</header>
